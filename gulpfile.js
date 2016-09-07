4711352868c5763369e782342b8b20ba03f61c1e
Aiman N <proprogrammer11@gmail.com>
2016-09-07 09:42:13 +0300

    Added a link to Admin/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,38 @@
 //Import the packages here
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+
 
 //Perform some tasks here
+
+gulp.task('css', function () {
+  return gulp.src(['assets/css/*.css', '!assets/css/_*.css'])
+    .pipe(plumber({ errorHandler: handleError }))
+    .pipe(autoprefixer({
+			browsers: ['> 5%'],
+			cascade: false}))
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(browserSync.stream());
+});
+
+function handleError(err) {
+  console.error(err.toString());
+  this.emit('end');
+};
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'css'], function(){
+  gulp.watch('assets/css/*.css', ['css']);
+  // gulp.watch('**/*.html', browserSync.reload);
+  // gulp.watch('assets/js/*.js', browserSync.reload);
+  // Other watchers
+})
