dd79e3b6aabf9a1efa012c675c9023e834869c78
Aiman N <proprogrammer11@gmail.com>
2016-09-16 23:57:04 +0300

    Fixed the collapse buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,11 +7,15 @@ jQuery(document).ready(function($) {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('.questions-list.list-group > .list-group-item').on('click', function(event) {
-    	$(this).parent().parent().hide().siblings().hide();
-    	$('.question-thread.row').removeClass('hidden').show();
+        $(this).parent().parent().hide().siblings().hide();
+        $('.question-thread.row').removeClass('hidden').show();
     });
     $('.discussion-back').on('click', function(event) {
-    	$('.question-thread.row').hide().siblings().show();
+        $('.question-thread.row').hide().siblings().show();
     });
 
+    $('body').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function(e) {
+        var $target = $(this).parent().next()
+        $target.collapse('toggle')
+    })
 });
